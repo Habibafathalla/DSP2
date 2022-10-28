90afcc20229e1d54619017c413ccfd337ae627dc
Adham <81479927+Adham-Mohammed@users.noreply.github.com>
2022-10-28 15:41:51 +0200

    initial submit

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import streamlit.components.v1 as com
+from streamlit_option_menu import option_menu
+from streamlit import button
+import streamlit as st
+import  streamlit_vertical_slider  as svs
+import numpy as np
+import scipy as sc
+from scipy.interpolate import interp1d, interp2d,splev
+import pandas as pd
+from math import ceil,floor
+import plotly.express as px
+from plotly.express import colors
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="equalizer", page_icon=":bar_chart:",layout="wide")
+
+
+#hiding copyright things
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+
+
+
+groups = [(0,200,100),
+            (0,200,150),
+            (0,200,75),
+            (0,200,25),
+            (0,200,150),
+            (0,200,60),
+            (0,200,86),
+            (0,200,150),
+            (0,200,150),
+            (0,200,25),
+            ]
+
+adjusted_data = []
+sliders = {}
+columns = st.columns(len(groups))
+for idx, i in enumerate(groups):
+    min_value = i[0]
+    max_value = i[1]
+    key = f'member{str(idx)}'
+    with columns[idx]:
+        sliders[f'slider_group_{key}'] = svs.vertical_slider(key=key, default_value=i[2], step=1, min_value=min_value, max_value=max_value)
+        if sliders[f'slider_group_{key}'] == None:
+            sliders[f'slider_group_{key}']  = i[1]
+        adjusted_data.append((i[0],sliders[f'slider_group_{key}'] ))
+
