5fc73eb5a80dce98ccade9a564f1340cc5c7f39c
Habibafathalla <habiba.elshenofy01@eng-st.cu.edu.eg>
2022-11-19 10:23:40 +0200

    Feat:Modes

--- Functions.py
+++ Functions.py
@@ -30,19 +30,18 @@ class Functions():
                     flat_list.append(item)
 
         return flat_list
-    # def final_func(fou_of_signal,frequencies,list_of_freqs,list_of_sliders):
-    #     final_fou = fou_of_signal
-    #     final_fou = Functions.modify_magnitude(fou_of_signal,frequencies,list_of_freqs,list_of_sliders)
-    #         # freqs_update = Functions.select_range(frequencies,list_of_freqs[iter][0],list_of_freqs[iter][1],True)
+
+    def final_func(fou_of_signal,frequencies,list_of_freqs,list_of_sliders):
+        final_fou = fou_of_signal
+        for iter in range(len(list_of_sliders)):
+            freqs_update = Functions.select_range(frequencies,list_of_freqs[iter][0],list_of_freqs[iter][1],True)
+            final_fou = Functions.modify_magnitude(freqs_update,fou_of_signal,list_of_sliders[iter])
+        return final_fou
         
-    #     return final_fou
-    def modify_magnitude(spectrum,frequencies,freq_list,factors):
-        cutoff=spectrum.copy()
-        W=frequencies
-        for i in range(len(factors)):
-            for r in freq_list[i]:
-                cutoff[(W<r[1]) & (W>r[0])] *= 10**(int(factors[i])*np.hanning(len(cutoff[(W>r[0])&(W<r[1])]))/20)
-        return  cutoff
+    def modify_magnitude(freq_list,list_freq_domain,factor):
+        list_freq_domain[freq_list] = list_freq_domain[freq_list] * (factor+1)
+        return list_freq_domain
+        
     def fourier_transformation(time_domain_data, sampling_rate):
         frequencies = np.fft.rfftfreq(len(time_domain_data), 1/sampling_rate)
         freq_domain_data = np.fft.rfft(time_domain_data)
@@ -54,7 +53,7 @@ class Functions():
             selected_freqs = (frequencies>=min)&(frequencies<=max)
         else:
             selected_freqs = (frequencies>min)&(frequencies<max)
-        return 
+        return selected_freqs
 
     def bins_separation(frequency, amplitude,sliders_number):
         freq_list = []
@@ -72,7 +71,7 @@ class Functions():
             values = []
             for i in range(0, sliders_number):
                 with columns[i]:
-                    value = svs.vertical_slider( key= i, default_value=0, step=1, min_value=-20, max_value=20)
+                    value = svs.vertical_slider(key= i, default_value=0.0, step=0.1, min_value=-1.0, max_value=1.0)
                     st.text(text[i])
                     if value == None:
                         value = 0.0
@@ -93,49 +92,79 @@ class Functions():
         inverse_combined=np.fft.irfft(combined)
         signal=np.real(inverse_combined)
         return signal
-
+        
     def plotSpectrogram(audioData, fs, Title):
-        N = 512
-        w = signal.blackman(N)
-        freqs, time, Pxx = signal.spectrogram(audioData, fs, window=w, nfft=N)
+            N = 512
+            w = signal.blackman(N)
+            freqs, time, Pxx = signal.spectrogram(audioData, fs, window=w, nfft=N)
 
-        layout = go.Layout(margin=go.layout.Margin(l=0, r=0, b=0, t=30,))
-        fig = go.Figure(layout=layout)
+            layout = go.Layout(margin=go.layout.Margin(l=0, r=0, b=0, t=30,))
+            fig = go.Figure(layout=layout)
 
-        fig.add_trace(go.Heatmap(x=time, y=freqs, z=10*np.log10(Pxx),
-                    colorscale='Jet', name='Spectrogram'))
+            fig.add_trace(go.Heatmap(x=time, y=freqs, z=10*np.log10(Pxx),
+                        colorscale='Jet', name='Spectrogram'))
 
-        fig.update_layout(height=300, title={
-            'text': Title,
-            'y': 1,
-            'x': 0.49,
-            'xanchor': 'center',
-            'yanchor': 'top'},
-            title_font=dict(
-            family="Arial",
-            size=17))
-        fig.update_xaxes(title='Time')
-        fig.update_yaxes(title='Frequency')
-        st.sidebar.plotly_chart(fig, use_container_width=True)
+            fig.update_layout(height=300, title={
+                'text': Title,
+                'y': 1,
+                'x': 0.49,
+                'xanchor': 'center',
+                'yanchor': 'top'},
+                title_font=dict(
+                family="Arial",
+                size=17))
+            fig.update_xaxes(title='Time')
+            fig.update_yaxes(title='Frequency')
+            st.sidebar.plotly_chart(fig, use_container_width=True)
 
+        
     def plot_animation(df):
-        brush = alt.selection_interval()
-        chart1 = alt.Chart(df).mark_line().encode(
-                x=alt.X('time', axis=alt.Axis(title='Time')),
-                # y=alt.Y('amplitude', axis=alt.Axis(title='Amplitude')),
-            ).properties(
-                width=500,
-                height=300
-            ).add_selection(
-                brush).interactive()
-        figure = chart1.encode(
-                    y=alt.Y('amplitude',axis=alt.Axis(title='Amplitude')))| chart1.encode(
-                    y=alt.Y('amplitude after processing',axis=alt.Axis(title='Amplitude after'))).add_selection(
-                brush)
-        return figure
+            brush = alt.selection_interval()
+            chart1 = alt.Chart(df).mark_line().encode(
+                    x=alt.X('time', axis=alt.Axis(title='Time')),
+                ).properties(
+                    width=500,
+                    height=300
+                ).add_selection(
+                    brush).interactive()
+            
+            figure = chart1.encode(
+                        y=alt.Y('amplitude',axis=alt.Axis(title='Amplitude')))| chart1.encode(
+                        y=alt.Y('amplitude after processing',axis=alt.Axis(title='Amplitude after Processing'))).add_selection(
+                    brush)
 
+            return figure
 
-    def plotShow(data, idata,resume_btn,sr):
+    def currentState(df, size, N):
+        if st.session_state.size1 == 0:
+            step_df = df.iloc[0:N]
+        # if st.session_state.flagStart == 0:
+        #     step_df = df.iloc[0:N]
+        if st.session_state.flag == 0:
+            step_df = df.iloc[st.session_state.i : st.session_state.size1 - 1]
+        lines = Functions.plot_animation(step_df)
+        line_plot = st.altair_chart(lines)
+        line_plot = line_plot.altair_chart(lines)  #
+        return line_plot
+
+    def plotRep(df, size, start, N, line_plot):
+        for i in range(start, N - size):  #
+                st.session_state.start=i 
+                st.session_state.startSize = i-1
+                step_df = df.iloc[i:size + i]
+                st.session_state.size1 = size + i
+                # st.session_state.i = i
+                lines = Functions.plot_animation(step_df)
+                line_plot.altair_chart(lines)
+                st.session_state.size1 = size + i
+                time.sleep(.1)   #
+        if st.session_state.size1 == N - 1:
+            st.session_state.flag =1
+            step_df = df.iloc[0:N]
+            lines = Functions.plot_animation(step_df)
+            line_plot.altair_chart(lines)
+
+    def plotShow(data, idata,pause_btn,value,sr):
         time1 = len(data)/(sr)
         if time1>1:
             time1 = int(time1)
@@ -147,149 +176,48 @@ class Functions():
         N = df.shape[0]  # number of elements in the dataframe
         burst = 10      # number of elements (months) to add to the plot
         size = burst 
-        step_df = df.iloc[:st.session_state.size1]
-        if st.session_state.size1 ==0:
-            step_df = df.iloc[0:N]
-        lines = Functions.plot_animation(step_df)
-        line_plot = st.altair_chart(lines)
-        line_plot= line_plot.altair_chart(lines)
+        line_plot = Functions.currentState(df, size, N)
+
+        if pause_btn:
+            st.session_state.flag = 0
+            st.session_state.pause_play_flag = not(st.session_state.pause_play_flag)
+            if st.session_state.pause_play_flag :
+                Functions.plotRep(df, size, st.session_state.start, N, line_plot)
         
-        if resume_btn: 
-            st.session_state.flag = not(st.session_state.flag)
-            if st.session_state.flag :
-                for i in range( st.session_state.start,N):
-                    st.session_state.start =i 
-                    step_df = df.iloc[i:size+i]
-                    lines =Functions. plot_animation(step_df)
-                    line_plot = line_plot.altair_chart(lines)
-                    st.session_state.size1 = size
-                    size = i + burst
-                    time.sleep(.1)
-        
-        if st.session_state.flag :
-            for i in range(st.session_state.start,N):
-                    st.session_state.start =i 
-                    step_df = df.iloc[i:size+i]
-                    lines = Functions.plot_animation(step_df)
-                    line_plot = line_plot.altair_chart(lines)
-                    st.session_state.size1 = size
-                    size = i + burst
-                    time.sleep(.1)
+        if st.session_state.pause_play_flag:
+            st.session_state.flag = 1
+            Functions.plotRep(df, size, st.session_state.start, N, line_plot)
+            
 
-# Second Graph
+# def time_stretch(y,rate):
 
+#     if rate <= 0:
+#         raise ParameterError("rate must be a positive number")
+#     stft = core.stft(y)
+#     stft_stretch = core.phase_vocoder(
+#         stft,
+#         rate=rate,
+#         hop_length=kwargs.get("hop_length", None),
+#         n_fft=kwargs.get("n_fft", None),
+#     )
 
-    # def plot_animation(df):
-    #     brush = alt.selection_interval()
-    #     chart1 = alt.Chart(df).mark_line().encode(
-    #             x=alt.X('time', axis=alt.Axis(title='Time')),
-    #             # y=alt.Y('amplitude', axis=alt.Axis(title='Amplitude')),
-    #         ).properties(
-    #             width=500,
-    #             height=300
-    #         ).add_selection(
-    #             brush).interactive()
-        
-    #     figure = chart1.encode(
-    #                 y=alt.Y('amplitude',axis=alt.Axis(title='Amplitude')))| chart1.encode(
-    #                 y=alt.Y('amplitude after processing',axis=alt.Axis(title='Amplitude after'))).add_selection(
-    #             brush)
+#     len_stretch = int(round(y.shape[-1] / rate))
+#     y_stretch = core.istft(stft_stretch, dtype=y.dtype, length=len_stretch, **kwargs)
 
-    #     return figure
+#     return y_stretch
 
-    # def plotShow(data, idata,start_btn,pause_btn,resume_btn,value,sr):
-
-    #     time1 = len(data)/(sr)
-    #     if time1>1:
-    #         time1 = int(time1)
-    #     time1 = np.linspace(0,time1,len(data))   
-    #     df = pd.DataFrame({'time': time1[::30], 
-    #                         'amplitude': data[:: 30],
-    #                         'amplitude after processing': idata[::30]}, columns=[
-    #                         'time', 'amplitude','amplitude after processing'])
-    #     N = df.shape[0]  # number of elements in the dataframe
-    #     burst = 10      # number of elements (months) to add to the plot
-    #     size = burst 
-        
-    #     step_df = df.iloc[0:st.session_state.size1]
-    #     if st.session_state.size1 ==0:
-    #         step_df = df.iloc[0:N]
-
-    #     lines = Functions.plot_animation(step_df)
-    #     line_plot = st.altair_chart(lines)
-    #     line_plot= line_plot.altair_chart(lines)
-
-    #     # lines = plot_animation(df)
-    #     # line_plot = st.altair_chart(lines)
-    #     N = df.shape[0]  # number of elements in the dataframe
-    #     burst = 10      # number of elements (months) to add to the plot
-    #     size = burst    #   size of the current dataset
-    #     if start_btn:
-    #         st.session_state.flag = 1
-    #         for i in range(1, N-burst):
-    #             st.session_state.start=i
-    #             step_df = df.iloc[i:size+i]
-    #             lines = Functions.plot_animation(step_df)
-    #             line_plot = line_plot.altair_chart(lines)
-    #             size = i + burst 
-    #             time.sleep(.1)
-
-    #     elif resume_btn: 
-    #             st.session_state.flag = 1
-    #             for i in range( st.session_state.start,N):
-    #                 st.session_state.start =i 
-    #                 step_df = df.iloc[i:size+i]
-    #                 lines = Functions.plot_animation(step_df)
-    #                 line_plot = line_plot.altair_chart(lines)
-    #                 st.session_state.size1 = size
-    #                 time.sleep(.1)
-
-    #     elif pause_btn:
-    #             st.session_state.flag =0
-    #             step_df = df.iloc[0:st.session_state.size1]
-    #             lines = Functions.plot_animation(step_df)
-    #             line_plot= line_plot.altair_chart(lines)
-
-
-
-    #     if st.session_state.flag == 1:
-    #         for i in range( st.session_state.start,N):
-    #                 st.session_state.start =i 
-    #                 step_df = df.iloc[i:size+i]
-    #                 lines = Functions.plot_animation(step_df)
-    #                 line_plot = line_plot.altair_chart(lines)
-    #                 st.session_state.size1 = size
-    #                 time.sleep(.1)
-
-def time_stretch(y,rate):
-
-    if rate <= 0:
-        raise ParameterError("rate must be a positive number")
-    stft = core.stft(y)
-    stft_stretch = core.phase_vocoder(
-        stft,
-        rate=rate,
-        hop_length=kwargs.get("hop_length", None),
-        n_fft=kwargs.get("n_fft", None),
-    )
-
-    len_stretch = int(round(y.shape[-1] / rate))
-    y_stretch = core.istft(stft_stretch, dtype=y.dtype, length=len_stretch, **kwargs)
-
-    return y_stretch
-
-def pitch_shift(y, sr, n_steps):
+# def pitch_shift(y, sr, n_steps):
     
-    if not util.is_positive_int(bins_per_octave):
-        raise ParameterError(f"bins_per_octave={bins_per_octave} must be a positive integer.")
+#     if not util.is_positive_int(bins_per_octave):
+#         raise ParameterError(f"bins_per_octave={bins_per_octave} must be a positive integer.")
 
-    rate = 2.0 ** (-float(n_steps) / bins_per_octave)
+#     rate = 2.0 ** (-float(n_steps) / bins_per_octave)
 
-    # Stretch in time, then resample
-    y_shift = core.resample(
-        time_stretch(y, rate=rate, **kwargs),
-        orig_sr=float(sr) / rate,
-        target_sr=sr,
-        res_type=res_type,
-    )
-    return util.fix_length(y_shift, size=y.shape[-1])
+#     # Stretch in time, then resample
+#     y_shift = core.resample(
+#         time_stretch(y, rate=rate, **kwargs),
+#         orig_sr=float(sr) / rate,
+#         target_sr=sr,
+#         res_type=res_type,
+#     )
+#     return util.fix_length(y_shift, size=y.shape[-1])
--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import streamlit.components.v1 as com
-from streamlit_option_menu import option_menu
-from streamlit import button
-import streamlit as st
 import  streamlit_vertical_slider  as svs
 import numpy as np
 import scipy as sc
@@ -84,8 +81,18 @@ if 'start' not in st.session_state:
     st.session_state['start']=0
 if 'size1' not in st.session_state:
     st.session_state['size1']=0
+if  'spectrum_inv_uniform' not in st.session_state:
+    st.session_state['spectrum_inv_uniform']=[]
+if "pause_play_flag" not in st.session_state:
+    st.session_state.pause_play_flag = False
+if 'i' not in st.session_state:
+    st.session_state['i']=0
 if 'flag' not in st.session_state:
     st.session_state['flag'] = 0
+if 'startSize' not in st.session_state:
+    st.session_state['startSize'] = 0    
+if 'flagStrat' not in st.session_state:
+    st.session_state['flagStart'] = 0
 
 select_col,graph=st.columns((1,4))
 
@@ -110,49 +117,25 @@ if Mode_Selection=='Musical Instruments' :
     sliders_number = 3
 
    
-    lst_flute =[(700,1600)]
-   
-    pluc = [(0.0, 100.0),(211.0, 311.0),
-        (472.0, 572.0),(737.0, 837.0),
-        (996.0, 1096.0),(1258.0, 1358.0),
-        (1517.0, 1617.0),(1780.0, 1880.0),
-        (2305.0, 2405.0),(2562.0, 2662.0),
-        (2828.0, 2928.0),(3091.0, 3191.0),
-        (3351.0, 3451.0),(3612.0, 3712.0),
-        (3874.0, 3974.0),(4139.0, 4239.0),
-        (4398.0, 4498.0),(4658.0, 4758.0),
-        (4921.0, 5021.0),(5179.0, 5279.0),
-        (5445.0, 5545.0),(5706.0, 5806.0),
-        (5967.0, 6067.0),(6023.0, 6123.0),
-        (6224.0, 6324.0),(6492.0, 6592.0),
-        (6751.0, 6851.0),(7010.0, 7110.0),
-        (7275.0, 7375.0),(7544.0, 7644.0),
-        (8067.0, 8167.0),(8839.0, 8939.0),
-        (9361.0, 9461.0),(9623.0, 9723.0),
-        (9894.0, 9994.0),(10152.0,10252.0)]
-    drip = [(463.0715281212067, 663.0715281212067),
-        (667.9449714516927, 867.9449714516927), 
-        (1187.0678415681339, 1387.0678415681339), 
-        (1389.9586386728408, 1589.9586386728408), 
-        (1918.003416805287, 2118.003416805287), 
-        (2146.99905588275, 2346.99905588275), 
-        (2649.93031515533, 2849.93031515533), 
-        (250, 750)]
-    lst_final_music=[lst_flute,pluc,drip]
-    text=["Flute","Pluc","drip"]
+    Drums =[0,279]
+    English_horn = [280, 1000]
+    Glockenspeil =[1000, 7000]
+
+    lst_final_music=[Drums,English_horn,Glockenspeil]
+    text=["Drums","English_horn","Glockenspeil"]
 
     flag=1
       
 if Mode_Selection=='Vowels' :
     sliders_number = 4
 
-    lst_s=[(0,2400)]
-    lst_o=[(0,890)]
-    # lst_h=[(103,183),(246,326),(401,481),(500,660),(687,767),(840,900)]
-    lst_h=[(103,183),(183,985),(1000,1888),(100,472)]
-    lst_e=[(82,182),(214,314),(2039,2639),(3156,3756)]
-    lst_final=[lst_o,lst_o,lst_h,lst_e]
-    text=["A","C","H","E"]
+    lst_z=[4500,8400]
+    lst_s=[4500,8400]
+    lst_a=[5000,700]
+    lst_e=[1600,1800]
+    lst_final=[lst_z,lst_s,lst_a,lst_e]
+    text=["Vowels","S","Vowels",'Vowels']
+
 
     flag=1
 
@@ -176,21 +159,18 @@ if  flag==1:
             st.audio(upload_file, format='audio/wav')
         else:
             input_bytes=Functions.convertToAudio(st.session_state['sampleRare'],st.session_state['audio'])
+
+            st.write('Original Audio')
             st.audio(input_bytes, format='audio/wav')
 
-    
-    c0,c1,c2,c3,c4,c5,c6=st.columns((1,2,3,4,5,6,7),gap="small")
-    with c4:   
-          resume= st.button('Play/Pause')
-   
+    with graph:
+
+        pause_btn= st.button("▷")
 
     
     # transform to fourier 
-    spectrum,frequencies, magnitude,phase, number_samples = Functions.fourier_transformation(st.session_state['audio'], st.session_state['sampleRare'])
-    st.write(len(st.session_state['audio']))
-    spTry,freqTry, magTry,phaseTry, number_samplesTry = Functions.fourier_transformation(st.session_state['audio'][0:2000], st.session_state['sampleRare'])
-
-    freq_axis_list, amplitude_axis_list,bin_max_frequency_value = Functions.bins_separation(frequencies, magnitude, sliders_number)
+    list_freq_domain,frequncies, magnitude,phase, number_samples = Functions.fourier_transformation(st.session_state['audio'], st.session_state['sampleRare'])    # spTry,freqTry, magTry,phaseTry, number_samplesTry = Functions.fourier_transformation(st.session_state['audio'][0:2000], st.session_state['sampleRare'])
+    freq_axis_list, amplitude_axis_list,bin_max_frequency_value = Functions.bins_separation(frequncies, magnitude, sliders_number)
     valueSlider = Functions.Sliders_generation(sliders_number,text)
     value=valueSlider[0]
     if Mode_Selection=="Voice Changer":
@@ -203,20 +183,21 @@ if  flag==1:
           st.audio(result_bytes, format='audio/wav')
     
        with graph:
-    #     resume= st.button('Play/Pause')
-    # plotShow(original_signal[:len(updated_signal)],updated_signal,resume,sample_rate )
-          Functions.plotShow(st.session_state['audio'], st.session_state['spectrum_inv'], resume,valueSlider,st.session_state['sampleRare'])
+          Functions.plotShow(st.session_state['audio'], st.session_state['spectrum_inv'], pause_btn,valueSlider,st.session_state['sampleRare'])
+
+
+
         
     else:  
         if Mode_Selection=="Uniform Range":
           Modified_signal=Functions.frequencyFunction(valueSlider, amplitude_axis_list) 
         elif Mode_Selection=="Vowels" :
-          Modified_signal=Functions.modify_magnitude(spectrum,frequencies,lst_final,valueSlider)
-          Modified_signaltry=Functions.modify_magnitude(spTry,freqTry,lst_final,valueSlider)
-          fig_trans=px.line(x=freqTry, y=np.abs(Modified_signaltry)).update_layout(yaxis_title='Amp',xaxis_title='HZ')
-          st.plotly_chart(fig_trans)
+            Modified_signal=Functions.final_func(list_freq_domain,frequncies,lst_final,valueSlider)
+        #   Modified_signaltry=Functions.modify_magnitude(spTry,freqTry,lst_final,valueSlider)
+            fig_trans=px.line(x=frequncies, y=np.abs(st.session_state['spectrum'])).update_layout(yaxis_title='Amp',xaxis_title='HZ')
+            st.plotly_chart(fig_trans)
         elif Mode_Selection=="Musical Instruments":
-          Modified_signal=Functions.modify_magnitude(spectrum,frequencies,lst_final_music,valueSlider)
+            Modified_signal=Functions.final_func(list_freq_domain,frequncies,lst_final_music,valueSlider)
 
         else:
             Modified_signal=magnitude
@@ -225,25 +206,13 @@ if  flag==1:
             st.session_state['spectrum_inv']=Functions.inverse(Modified_signal,phase) 
         elif Mode_Selection== "Vowels" or" Musical Instruments":
             st.session_state['spectrum_inv']=np.fft.irfft(Modified_signal)
-        # if Mode_Selection=="Uniform Range"or"Vowels" or" Musical Instruments":
-        #     st.session_state['spectrum_inv']=np.fft.irfft(Modified_signal)
 
-        # elif Mode_Selection=="Voice Changer":
-        #     flag==1
-        #     st.session_state['spectrum_inv']=librosa.effects.pitch_shift(st.session_state['audio'] , sr= st.session_state['sampleRare'] , n_steps=valueSlider)
-    
         #convert to audio
         result_bytes = Functions.convertToAudio(st.session_state['sampleRare'], st.session_state['spectrum_inv'])
         with graph:
-            Functions.plotShow(st.session_state['audio'], st.session_state['spectrum_inv'], resume,st.session_state['sampleRare'])
-            #ranges
-            # fig_2= px.line(x=st.session_state['fft_frequency'], y=Modified_signal)
-            # st.plotly_chart(fig_2,use_container_width=True)
+            Functions.plotShow(st.session_state['audio'], st.session_state['spectrum_inv'], pause_btn,valueSlider,st.session_state['sampleRare'])
         with select_col:
+            st.write('Modified Audio')
+
             st.audio(result_bytes, format='audio/wav')
-    
-
-           
-       
-
-
+         
